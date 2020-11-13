faa2cb8d70158802bda93ff2b6bdc929391e2505
zieng12161121 <74398076+zieng12161121@users.noreply.github.com>
2020-11-13 16:59:01 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+function pick1(){
+       $('html, body').animate({scrollTop: 740}, 200);
+}
+function pick2(){
+       $('html, body').animate({scrollTop: 1463}, 200);
+}
+function pick3(){
+        $('html, body').animate({scrollTop: 2189}, 200);
+}
+function pick4(){
+    $('html, body').animate({scrollTop: 2910}, 200);
+}
+function pick5(){
+    $('html, body').animate({scrollTop: 440}, 200);
+}
+function pick6(){
+    $('html, body').animate({scrollTop: 1243}, 200);
+}
+function pick7(){
+     $('html, body').animate({scrollTop: 2038}, 200);
+}
+function pick8(){
+ $('html, body').animate({scrollTop: 2838}, 200);
+}
+function pick9(){
+    $('html, body').animate({scrollTop: 740}, 200);
+}
+function pick10(){
+    $('html, body').animate({scrollTop: 1463}, 200);
+}
+function pick11(){
+     $('html, body').animate({scrollTop: 2189}, 200);
+}
+function pick12(){
+ $('html, body').animate({scrollTop: 2908}, 200);
+}
+function totop() {
+
+    var $win = $(window);
+    
+    var $backToTop = $('.js-back-to-top');
+    
+    // 當用戶滾動到離頂部100像素時，展示回到頂部按鈕
+    
+    $win.scroll(function () 
+    {    
+        if ($win.scrollTop() > 100) 
+        {    
+            $backToTop.show();    
+        } 
+        else 
+        {    
+            $backToTop.hide();    
+        }    
+    })
+}
+
+
+ function picktop(){
+
+    $('html, body').animate({scrollTop: 0}, 1000);
+    
+    }
+
+
+    
+    
+    
+   
